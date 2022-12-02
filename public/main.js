b786e2e74034bbcde0efc8fdf22a1deb1077f4ef
Aaron Cil <cilaaron27@gmail.com>
2022-12-01 19:52:07 -0500

    added frontend to show city of search

--- public/main.js
+++ public/main.js
@@ -1,6 +1,5 @@
 // Upon the page loading call the 'fetchapi function'
 document.addEventListener('DOMContentLoaded', fetchApi)
-
 function fetchApi() {
     const options = {
         method: 'GET',
@@ -19,6 +18,7 @@ function handleFetch(data) {
     let nd = document.getElementById('ND')
     let oz = document.getElementById('OZ')
     let sd = document.getElementById('SD')
+    let ct = document.getElementById('cityName')
     function handleError(){
         if ( data.overall_aqi=== undefined) {
             return 'Please try something else your input may not be supported'
@@ -28,17 +28,20 @@ function handleFetch(data) {
     }
     
     aqi.innerHTML = handleError()
+    ct.innerHTML = handleCity()
     cm.innerHTML = `Carbon monoxide (CO): ${data.CO.concentration}`
     nd.innerHTML = `Nitrogen dioxide (NO2): ${data.NO2.concentration}`
     oz.innerHTML = `Ozone (O3):${data.O3.concentration}`
     sd.innerHTML = `Sulphur dioxide (SO2): ${data.SO2.concentration}`
 }
+
 const input = document.getElementById("searchInput");
 input.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
         handleSearch(event)
     }
 });
+
 function handleSearch(event) {
     let city = input.value
     const options = {
@@ -53,3 +56,11 @@ function handleSearch(event) {
         .catch(err => console.error(err));
 }
 
+function handleCity(){
+    if (input.value == ""){
+        input.value = "London"
+        return input.value;
+    } else {
+        return `City: ${input.value}`;
+    }
+}
